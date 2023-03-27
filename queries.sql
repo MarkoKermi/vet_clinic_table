b1006aae5571b3cd89e4f86cebbac4a469b1f4d0
Marko Kermiciev <kermicievmarko@gmail.com>
2023-03-27 22:20:41 +0200

    additional changes

--- queries.sql
+++ queries.sql
@@ -2,10 +2,10 @@
 
 SELECT * from animals WHERE name Like '%mon';
 select name from animals WHERE date_of_birth between '2016-01-01' and '2019-01-01';
-select name from animals WHERE neutered=true and escape_attempts<3
-select date_of_birth from animals WHERE name in ('Agumon', 'Pikachu')
-select name, escape_attempts from animals WHERE weight_kg >10.5
-select * from animals WHERE neutered=true
+select name from animals WHERE neutered=true and escape_attempts<3;
+select date_of_birth from animals WHERE name in ('Agumon', 'Pikachu');
+select name, escape_attempts from animals WHERE weight_kg >10.5;
+select * from animals WHERE neutered=true;
 select * from animals WHERE name !='Gabumon';
 select * from animals WHERE weight_kg >= 10.4 and weight_kg <= 17.3;
 
