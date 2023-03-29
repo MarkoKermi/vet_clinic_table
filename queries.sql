cfd215c0e816e3e54f83dd5f703494ac6964838c
Marko Kermiciev <kermicievmarko@gmail.com>
2023-03-29 12:36:28 +0200

    setup multiple tables

--- queries.sql
+++ queries.sql
@@ -16,3 +16,12 @@ select avg(weight_kg) from animals;
 select neutered, count(neutered) as result from animals group by neutered order by result desc limit 1;
 select species, MAX(weight_kg) as max_weight, MIN(weight_kg) as min_weight from animals group by species;
 select species,avg(escape_attempts) from animals where date_of_birth between '1900-01-01' and '2000-12-31' group by species;
+
+
+SELECT name as animal_name, full_name FROM animals A JOIN owners O ON A.owner_id = O.id WHERE full_name = 'Melody Pond';
+SELECT A.name as animal_name, S.name as specie_type FROM animals A JOIN species S ON A.species_id = S.id WHERE S.name = 'Pokemon';
+SELECT full_name, name as animal_name FROM animals A RIGHT JOIN owners O ON A.owner_id = O.id ; 
+SELECT species.name, COUNT(*) FROM animals JOIN species ON animals.species_id = species.id GROUP BY species.name;
+SELECT full_name, A.name FROM animals A JOIN owners O ON A.owner_id = O.id WHERE full_name = 'Jennifer Orwell';
+SELECT full_name, A.name FROM animals A JOIN owners O ON A.owner_id = O.id WHERE full_name = 'Dean Winchester' AND escape_attempts < 1;
+SELECT  full_name, COUNT (owner_id) AS total FROM animals A JOIN owners O ON A.owner_id = O.id GROUP BY O.full_name ORDER BY total DESC LIMIT 1; 
